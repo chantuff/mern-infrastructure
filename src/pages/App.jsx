b2b334e6de55a488be1b0a0eb2a131cbb1cb98cf
chantuff <chan.tuff@yahoo.com>
2024-01-04 11:17:57 -0500

    routes working

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,18 +1,26 @@
+// import depedencies
 import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
 import AuthPage from "./AuthPage";
-import "./App.css";
 import NewOrderPage from "./NewOrderPage";
 import OrderHistoryPage from "./OrderHistoryPage";
 
+import "./App.css";
+
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(1);
 
   return (
     <>
-      Hello
+     
       {user ? (
         <>
-          <NewOrderPage />
+        Hello
+          <Routes>
+            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders" element={<OrderHistoryPage />} />
+          </Routes>
         </>
       ) : (
         <AuthPage />
